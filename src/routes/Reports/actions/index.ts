5f93ae9db4ab53a1263cb07bdba2233f5736dfbe
dangloc6789 <83728109+dangloc6789@users.noreply.github.com>
2022-06-03 00:24:35 +0700

    6

--- src/routes/Reports/actions/index.ts
+++ src/routes/Reports/actions/index.ts
@@ -1,14 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 import reportsReducer, { reportType } from "../reducer/reportsReducer"
 
-const getReport = createAsyncThunk("device/getAll", async () => {
+const getReport = createAsyncThunk("report/getAll", async () => {
   //fake get data from server
   const promise:Promise<Array<reportType>> = new Promise((resolve,reject) => {
     setTimeout(()=>resolve((reportsReducer.getInitialState().data)),1000)
   })
   return await promise
 })
-const updateReport = createAsyncThunk("device/update", async (formdata: reportType) => {
+const updateReport = createAsyncThunk("report/update", async (formdata: reportType) => {
   //fake put data
   const promise:Promise<reportType> = new Promise((resolve,reject) => {
     setTimeout(()=>resolve(formdata),1000)
